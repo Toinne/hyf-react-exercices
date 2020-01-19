d3a3d58542b58becb048f79b3b8972c2f071b6a5
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2020-01-19 19:52:44 +0100

    Document correct test file to edit

--- src/component.test.js
+++ src/component.test.js
@@ -7,7 +7,7 @@ import { render } from '@testing-library/react';
 // https://reactjs.org/docs/components-and-props.html
 
 // Fix the following tests
-// For some tests you might need to adjust something in `jsx.js` others require you to fix the assertion.
+// For some tests you might need to adjust something in `component.js` others require you to fix the assertion.
 // No need to change anything in tests that pass these are purely informative and are meant to teach you something.
 
 test('The component App exist and can render', () => {
