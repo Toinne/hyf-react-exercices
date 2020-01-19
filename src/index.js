c0afcffa01dfc5760cb524ca46d85c78dd3409af
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2020-01-19 19:42:27 +0100

    Add exercises for rendering/mounting a component

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './component'
+
+// Exercise 1:
+
+// Start the development server for your app
+// `yarn start`
+
+// Exercise 2:
+
+// Render the MyApp component you made in `component.js` instead of the App component
+
+// Exercise 3:
+
+// Bundle your app for deployment
+// `yarn build`
+
+ReactDOM.render(<App />, document.getElementById('root'));
